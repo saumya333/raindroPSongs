d5b52d0f3b8e9ab83bf3e08ed2327e9528905646
saumya333 <74599266+saumya333@users.noreply.github.com>
2023-04-11 00:56:24 +0530

    Delete src directory

--- src/screens/favorites/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Favorites() {
-  return (
-    <div className="screen-container">
-      favorites
-    </div>
-  )
-}
--- src/screens/trending/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Trending() {
-  return (
-    <div className="screen-container">
-      trending
-    </div>
-  )
-}
